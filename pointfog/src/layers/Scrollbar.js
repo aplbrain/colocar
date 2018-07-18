74825ef7c51132a7facbd5b2d801fe47914ed9d9
Jordan Matelsky <j6k4m8@gmail.com>
2018-07-18 14:05:38 -0400

    add point renders to scrollbar

--- pointfog/src/layers/Scrollbar.js
+++ pointfog/src/layers/Scrollbar.js
@@ -4,32 +4,66 @@ import type {
     P5Type
 } from "../types/p5Types";
 import type ImageManager from "./ImageManager";
+import type PointcloudManager from "./PointcloudManager";
 
 
 export default class Scrollbar {
 
     p: any;
     im: ImageManager;
+    pm: PointcloudManager;
     visible: boolean;
+
+    left: number;
+    top: number;
+    width: number;
+    height: number;
+
 
     constructor(opts: {
         p: P5Type,
-        imageManager: ImageManager
+        imageManager: ImageManager,
+        pointcloudManager: pointcloudManager
     }) {
         this.p = opts.p;
         this.im = opts.imageManager;
+        this.pm = opts.pointcloudManager;
+
+        this.left = 20;
+        this.top = 30;
+        this.height = this.p.height / 2;
+        this.width = 20;
     }
 
     mouseClicked(): void {}
     mousePressed(): void {}
+
+    getEntities() {
+        return this.pm.nodes.map(i => {
+            return {
+                z: i.z,
+                color: [0, 255, 255]
+            };
+        });
+    }
 
     draw(): void {
         this.p.rectMode(this.p.CORNER);
         this.p.noFill();
         this.p.stroke(255);
         this.p.strokeWeight(4);
-        this.p.rect(20, 30, 20, this.p.height/2);
+        this.p.rect(this.left, this.top, this.width, this.height);
         this.p.fill(200);
-        this.p.rect(20, 30, 20, this.p.height/2 * (this.im.currentZ / this.im.imageURIs.length));
+        this.p.rect(this.left, this.top, this.width, this.height * (this.im.currentZ / this.im.imageURIs.length));
+        this.getEntities().forEach(e => {
+            this.p.stroke(...e.color, 50);
+            let _z = this.top + (this.height * (e.z / this.im.imageURIs.length));
+            this.p.line(
+                this.left,
+                _z,
+                this.width + this.left,
+                _z
+            );
+        });
     }
 }
