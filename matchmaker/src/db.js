010e24488636ed54603223d711812952f8d4f0ce
Joseph Downs <Joseph.Downs@jhuapl.edu>
2018-07-26 08:14:19 -0400

    feat: use direct graph id lookup for pagination

--- matchmaker/src/db.js
+++ matchmaker/src/db.js
@@ -32,40 +32,46 @@ class Colocard implements Database {
     }
 
     getGraphsAndVolume(graphIdA: string, graphIdB: string) {
-        return fetch(`${this.url}/graphs?q={"namespace": "${this.matchmaker_name}", "active": true}`, {
+        let graphMetaA: Object;
+        let graphMetaB: Object;
+        return fetch(`${this.url}/graphs/${graphIdA}`, {
             headers: this.headers,
             method: "GET"
-        }).then(res => this._onQuestionSuccess(res, graphIdA, graphIdB)).catch(err => this._onException(err));
+        }).then(res => res.json()).then(json => {
+            graphMetaA = json;
+            return fetch(`${this.url}/graphs/${graphIdB}`, {
+                headers: this.headers,
+                method: "GET"
+            });
+        }).then(res => res.json()).then(json => {
+            graphMetaB = json;
+            return this._onGraphSuccess(graphMetaA, graphMetaB);
+        }).catch(err => this._onException(err));
     }
 
-    _onQuestionSuccess(res: Response, graphIdA: string, graphIdB: string): Promise<Question> {
-        return res.json().then((json: any) => {
-            console.log(json);
-            let graphMetaA: Object = json.filter(g => g._id === graphIdA)[0];
-            let graphMetaB: Object = json.filter(g => g._id === graphIdB)[0];
-            let volume: Object;
-            console.log(graphMetaA);
-            console.log(graphMetaB);
-            console.assert(graphMetaA.volume === graphMetaB.volume);
-            let volumeId: string = graphMetaA.volume;
-            let graphA = graphMetaA.structure;
-            let graphB = graphMetaB.structure;
+    _onGraphSuccess(graphMetaA: Object, graphMetaB: Object): Promise<Question> {
+        let volume: Object;
+        console.log(graphMetaA);
+        console.log(graphMetaB);
+        console.assert(graphMetaA.volume === graphMetaB.volume);
+        let volumeId: string = graphMetaA.volume;
+        let graphA = graphMetaA.structure;
+        let graphB = graphMetaB.structure;
 
-            return fetch(`${this.url}/volumes/${volumeId}`, {
-                headers: this.headers
-            }).then((res: Response) => res.json()).then((json: any) => {
-                volume = json;
-                let splitUri = volume.uri.split('/');
-                let nUri = splitUri.length;
-                volume.collection = splitUri[nUri - 3];
-                volume.experiment = splitUri[nUri - 2];
-                volume.channel = splitUri[nUri - 1];
-                return {
-                    graphA,
-                    graphB,
-                    volume
-                };
-            });
+        return fetch(`${this.url}/volumes/${volumeId}`, {
+            headers: this.headers
+        }).then((res: Response) => res.json()).then((json: any) => {
+            volume = json;
+            let splitUri = volume.uri.split('/');
+            let nUri = splitUri.length;
+            volume.collection = splitUri[nUri - 3];
+            volume.experiment = splitUri[nUri - 2];
+            volume.channel = splitUri[nUri - 1];
+            return {
+                graphA,
+                graphB,
+                volume
+            };
         });
     }
 
