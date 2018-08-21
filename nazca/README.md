dda57d6ddcaefd2308907458c9f0590db7219c51
Joseph Downs <Joseph.Downs@jhuapl.edu>
2018-08-21 09:31:08 -0400

    feat: add nazca, a bc clone to do trace-ynm

---
+++ nazca/README.md
@@ -0,0 +1,71 @@
+<p align=center><img align=center src='./logo.png' width=100 /></p>
+<h3 align=center>n a z c a</h3>
+<h6 align=center>simple and scalable graph validation</h6>
+
+<p align=center><img align=center src="https://img.shields.io/badge/all_contributors-3-orange.svg?style=flat-square" /></p>
+
+## Installation
+
+```shell
+$ yarn
+```
+
+## Running a development server
+
+```shell
+$ yarn start
+```
+
+This will run a development server in your shell (with linting and transpile outputs), and will automatically open your browser to http://localhost:3000 where it will display the running nazca app. Saving files to the nazca filesystem will trigger a rebuild and will restart the server (refreshing your page).
+
+## Usage
+
+### Mouse
+
+| Command | Description |
+|---------|-------------|
+| <kbd>LMB</kbd> | Click to place a new node (Trace Mode only) |
+| <kbd>RMB</kbd> | Drag to pan around the scene. Click to select a node |
+| <kbd>MMB</kbd> | Scroll to scrub through the z-stack |
+
+### Keyboard
+
+| Command | Description |
+|---------|-------------|
+| <kbd>q</kbd>, <kbd>e</kbd> | Pan up and down in the z-stack |
+| <kbd>+</kbd>, <kbd>-</kbd> | Zoom in and out |
+| <kbd>a</kbd> | Mark the current node as an Axon |
+| <kbd>d</kbd> | Mark the current node as a Dendrite |
+| <kbd>!</kbd> | Mark the current node as a bookmark, to return to later. (Press again to toggle) |
+| <kbd>@</kbd> | Return to the previous bookmark |
+| <kbd>delete</kbd> | Delete the currently active node |
+| <kbd>esc</kbd> | Reset the zoom and pan of the scene |
+| <kbd>T</kbd> | Toggle trace visibility |
+
+## Contributors
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore -->
+| [<img src="https://avatars2.githubusercontent.com/u/693511?v=4" width="100px;"/><br /><sub><b>Jordan Matelsky</b></sub>](http://jordan.matelsky.com)<br />[💻](https://github.com/aplbrain/colocar/commits?author=j6k4m8 "Code") [💬](#question-j6k4m8 "Answering Questions") | [<img src="https://avatars0.githubusercontent.com/u/9058954?v=4" width="100px;"/><br /><sub><b>Tucker Chapin</b></sub>](http://tuckerchap.in)<br />[💻](https://github.com/aplbrain/colocar/commits?author=tuckerchapin "Code") | [<img src="https://avatars0.githubusercontent.com/u/7283561?v=4" width="100px;"/><br /><sub><b>Joe Downs</b></sub>](https://github.com/jtpdowns)<br />[💻](https://github.com/aplbrain/colocar/commits?author=jtpdowns "Code") |
+| :---: | :---: | :---: |
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+<p align=center>[<a href="https://github.com/kentcdodds/all-contributors#emoji-key">emoji key</a>]</p>
+
+## Deploying
+
+- **Build the application.**
+
+```shell
+yarn build
+```
+
+- **Zip the build.**
+
+```shell
+zip -r build.zip build/*
+```
+
+- **Upload the build.**
+
+You'll need access to the AWS console in order to do this: Get in touch with a project maintainer.
---
+++ nazca/src/db.js
@@ -0,0 +1,146 @@
+// @flow
+
+import type {Question} from "./types/colocardTypes";
+import Config from "./_config";
+
+interface Database {
+    getNextQuestion(string, string): Promise<Object>;
+    postGraph(string, string, Object, string): any;
+}
+
+
+class Colocard implements Database {
+
+    url: string;
+    headers: Object;
+    breadcrumbs_name: string;
+
+    constructor(opts?: {url?: string}) {
+        /*
+        Create a new Colocard client.
+
+        Arguments:
+        opts: Object. Should include url (str)
+
+        */
+        opts = opts || {};
+        this.url = opts.url || Config.colocardUrl;
+        this.headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+        this.breadcrumbs_name = "breadcrumbs";
+    }
+
+    getNextQuestion(user: string, type: string) {
+        let openPromise = fetch(`${this.url}/questions?q={"active": true, "assignee": "${user}", "namespace": "${type}", "status": "open"}`, {
+            headers: this.headers,
+            method: "GET"
+        });
+        let pendingPromise = fetch(`${this.url}/questions?q={"active": true, "assignee": "${user}", "namespace": "${type}", "status": "pending"}&sort=-priority`, {
+            headers: this.headers,
+            method: "GET"
+        });
+        return Promise.all(
+            [openPromise, pendingPromise]
+        ).then(resList => this._onQuestionSuccess(resList)
+        ).catch(err => this._onException(err));
+    }
+
+    _onQuestionSuccess(resList: Array<Response>): Promise<Question> {
+        let jsonList = resList.map(res => res.json());
+        let questionPromise = Promise.all(jsonList).then(questionsList => {
+            let openQuestions: Array<Question> = questionsList[0];
+            let pendingQuestions: Array<Question> = questionsList[1];
+            let question;
+            if (openQuestions.length > 0) {
+                question = openQuestions[0];
+            } else {
+                if (pendingQuestions.length > 0) {
+                    question = pendingQuestions[0];
+                } else {
+                    throw new Error("you don't have any open or pending questions - ask an admin");
+                }
+            }
+            let volume = question.volume;
+            let splitUri = volume.uri.split('/');
+            let nUri = splitUri.length;
+            volume.collection = splitUri[nUri-3];
+            volume.experiment = splitUri[nUri-2];
+            volume.channel = splitUri[nUri-1];
+            let graphId = question.instructions.graph;
+            let graphPromise = fetch(`${this.url}/graphs/${graphId}`, {
+                headers: this.headers
+            }).then((res: Response) => res.json());
+            let fullQuestionPromise = graphPromise.then((graph: any) => {
+                question.instructions.graph = graph;
+                let statusPromise = this._setOpenStatus(question);
+                return statusPromise.then(() => {
+                    return {question, volume};
+                });
+            });
+            return fullQuestionPromise;
+        });
+        return questionPromise;
+    }
+
+    _setOpenStatus(question: Question) {
+        return fetch(`${this.url}/questions/${question._id}/status`, {
+            headers: this.headers,
+            method: "PATCH",
+            body: JSON.stringify({"status": "open"})
+        });
+    }
+
+    _onException(reason: any) {
+        console.log(reason);
+    }
+
+    postGraph(author: string, parent: string, structure: Object, volume: string): Promise<string> {
+        /*
+        Post a graph to the colocard API.
+
+        Arguments:
+        graph (Object): The graph to post. Should be fully
+        well-formed graph object
+
+        */
+        structure.multigraph = structure.options.multigraph;
+        structure.directed = structure.options.directed;
+        structure.links = structure.edges;
+        delete structure.edges;
+        delete structure.options;
+
+
+        return fetch(`${this.url}/graphs`, {
+            headers: this.headers,
+            method: "POST",
+            body: JSON.stringify({
+                author: author,
+                namespace: this.breadcrumbs_name,
+                parent: parent,
+                structure: structure,
+                volume: volume
+            })
+        }).then(values => {
+            console.log(values);
+            return "completed";
+        }).catch(reason => {
+            console.log(reason);
+            return "errored";
+        });
+    }
+
+    updateQuestionStatus(questionId: string, status: string): Promise<Response> {
+        return fetch(`${this.url}/questions/${questionId}/status`, {
+            headers: this.headers,
+            method: "PATCH",
+            body: JSON.stringify({status})
+        });
+    }
+
+}
+
+export {
+    Colocard
+};
