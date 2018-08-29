fde4d831248a0a75b3b0e7a5ec14c0d76e472e43
Jordan Matelsky <j6k4m8@gmail.com>
2018-08-29 15:58:21 -0400

    ❤️ Begin colocorazon

---
+++ colocorazon/types/p5.js
@@ -0,0 +1,75 @@
+// @flow
+
+export type P5Image = {
+    height: number,
+    width: number,
+};
+
+export type P5Type = {
+
+    width: number,
+    height: number,
+
+    windowWidth: number,
+    windowHeight: number,
+
+    createCanvas: (width: number, height: number) => Object,
+    resizeCanvas: (width: number, height: number) => Object,
+    createGraphics: Function,
+    frameRate: (number) => void,
+    canvas: Object,
+
+    // Drawing
+    background: (r: number, g: ? number, b : ? number, a : ? number) => void,
+    fill: (r: number, g: ? number, b : ? number, a : ? number) => void,
+
+    line: (x0: number, y0: number, x1: number, y1: number) => void,
+
+    ellipse: (number, number, number, number) => void,
+    rect: (number, number, number, number) => void,
+    rectMode: Function,
+
+    stroke: (r: number, g: ? number, b : ? number, a : ? number) => void,
+    noStroke: () => void,
+    strokeWeight: (number) => void,
+
+
+    // Core
+    setup: () => void,
+    draw: () => void,
+    clear: () => void,
+
+    // Events
+    windowResized: Function,
+
+    mousePressed: Function,
+    mouseClicked: Function,
+    mouseDragged: Function,
+
+    keyTyped: Function,
+    keyPressed: Function,
+    key: string,
+    keyCode: number,
+    // Mouse:
+    mouseWheel: Function,
+    mouseX: number,
+    mouseY: number,
+    pmouseX: number,
+    pmouseY: number,
+
+    RIGHT: number,
+    CENTER: number,
+
+    // Image
+    loadImage: (string, Function, Function, Object) => P5Image,
+    Image: P5Image,
+    image: (Object, number, number, number, number) => void,
+    imageMode: Function,
+
+    // Geometry
+    dist: (x0: number, y0: number, x1: number, y1: number) => number,
+
+    text: Function,
+    textSize: Function,
+    textAlign: Function,
+};
