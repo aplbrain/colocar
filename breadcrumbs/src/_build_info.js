a621bff432f7ab8fc1d2f9e1b3f409e36c97fcf9
Joseph Downs <Joseph.Downs@jhuapl.edu>
2018-11-07 15:51:38 -0500

    doc: update build info for apps with overlay toggle

--- breadcrumbs/src/_build_info.js
+++ breadcrumbs/src/_build_info.js
@@ -1,5 +1,5 @@
 export default {
-        "commit": "30ad269be96b010ed9038c8f81540cf399fe0ed3",
+        "commit": "e5d2cbf32456ffa146f006e096c994f2bda9b9de",
         "user": "downsjt1",
-        "creation": new Date(1538410726178),
+        "creation": new Date(1541623859298),
     };
