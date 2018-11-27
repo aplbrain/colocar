9fb7142240c3175735a6c751efdcfa4aca5f8984
Joseph Downs <Joseph.Downs@jhuapl.edu>
2018-11-27 15:50:57 -0500

    feat: update build info with status fix

--- nazca/src/_build_info.js
+++ nazca/src/_build_info.js
@@ -1,5 +1,5 @@
 export default {
-        "commit": "eb91c4aa103dfe5060845d76332dbaf4b7fdf130",
+        "commit": "bb2c441ac33168b19733f35c03778139aac7f29d",
         "user": "downsjt1",
-        "creation": new Date(1538411912282),
+        "creation": new Date(1543351749863),
     };
